5f025f35c0aa0e453511458cca128d8c7602f0b7
Kirill Cherkashin <kirill@kcherkashin.org>
2016-09-18 16:21:13 -0400

    Log answer times

--- kirjs-numbers/src/main/java/com/kirjs/numbers/questions/MultiplyQuestion.java
+++ kirjs-numbers/src/main/java/com/kirjs/numbers/questions/MultiplyQuestion.java
@@ -1,8 +1,20 @@
 package com.kirjs.numbers.questions;
 
+import com.kirjs.numbers.answers.Answer;
+import com.kirjs.numbers.answers.AnswerLog;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MultiplyQuestion implements Question {
+    private final AnswerLog answerLog;
     private int number1;
     private int number2;
+    long startTime;
+
+    public MultiplyQuestion() {
+        this.answerLog = new AnswerLog();
+    }
 
     @Override
     public boolean hasNext() {
@@ -11,13 +23,18 @@ public class MultiplyQuestion implements Question {
 
     @Override
     public String next() {
+        startTime = System.currentTimeMillis();
         number1 = 5 + (int) Math.ceil(Math.random() * 20);
         number2 = 5 + (int) Math.ceil(Math.random() * 20);
-        return String.valueOf(number1) + " * " + String.valueOf(number2);
+        return String.valueOf(number1) + " xx " + String.valueOf(number2);
     }
 
     @Override
     public boolean validate(String question) {
-        return !question.isEmpty() && Integer.valueOf(question) == number1 * number2;
+        boolean isValid = !question.isEmpty() && Integer.valueOf(question) == number1 * number2;
+        if(isValid){
+            answerLog.add(new Answer(number1, number2, System.currentTimeMillis() - startTime));
+        }
+        return isValid;
     }
 }
