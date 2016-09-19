bdcd059e793d52a96a22da4a098335a0fc5577c6
Kirill Cherkashin <kirill@kcherkashin.org>
2016-09-18 20:16:52 -0400

    clean up

--- kirjs-numbers/src/main/java/com/kirjs/numbers/MainActivity.java
+++ kirjs-numbers/src/main/java/com/kirjs/numbers/MainActivity.java
@@ -44,7 +44,6 @@ public class MainActivity extends AppCompatActivity {
         displayQuestion(question.next());
 
 
-        stats.add("hello");
         this.getStatsField().setAdapter(adapter);
 
         getAnswerField().addTextChangedListener(new QuestionTextWatcher(this));
